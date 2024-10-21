aa89743deb91a2b40468e732be2aec98ec54cd4d
Tiffany <tgateley22@gmail.com>
2024-10-21 08:02:18 -0500

    Initial Commit

---
+++ filesharer.py
@@ -0,0 +1,14 @@
+import os
+
+from filestack import Client
+
+
+class FileSharer:
+    def __init__(self, filepath, api_key=os.getenv("File_share_api")):
+        self.filepath = filepath
+        self.api_key = api_key
+
+    def share(self):
+        client = Client(self.api_key)
+        new_filelink = client.upload(filepath=self.filepath)
+        return new_filelink
