625ce8403f55bc88fe1e71706054d62c8a964fef
Tiffany <tgateley22@gmail.com>
2024-10-24 09:39:15 -0500

    Implement CameraScreen class. Start ImageScreen

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
-import requests
-
+import time
+from filesharer import FileSharer
 
 Builder.load_file('frontend.kv')
 
 
 class CameraScreen(Screen):
     def start(self):
-        pass
+        self.ids.camera.play = True
+        self.ids.start_stop.text = "Stop Camera"
 
     def stop(self):
-        pass
+        self.ids.camera.play = False
+        self.ids.start_stop.text = "Start Camera"
+        self.ids.camera.texture = None
 
     def capture(self):
-        pass
+        current_time = time.strftime('%Y%m%d-%H%M%S')
+        self.filepath = "pictures/" + current_time + ".png"
+        self.ids.camera.export_to_png(self.filepath)
+        self.manager.current = 'image_screen'
+        self.manager.current_screen.ids.img.source = self.filepath
 
 
 class ImageScreen(Screen):
-    pass
+    def create_link(self):
+        file_path = App.get_running_app().root.ids.camera_screen.filepath
+
+
 
 
 class RootWidget(ScreenManager):
