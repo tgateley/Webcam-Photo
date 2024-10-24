4cd998516ba53b7fba498f524ce0e2ff1f6963d6
Tiffany <tgateley22@gmail.com>
2024-10-24 10:25:20 -0500

    add open_link() method and add docstrings to all methods

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.core.clipboard import Clipboard
+import webbrowser
 import time
 from filesharer import FileSharer
 
@@ -10,15 +11,21 @@ Builder.load_file('frontend.kv')
 
 class CameraScreen(Screen):
     def start(self):
+        """Starts the webcam"""
         self.ids.camera.play = True
         self.ids.start_stop.text = "Stop Camera"
 
     def stop(self):
+        """Stops the webcam"""
         self.ids.camera.play = False
         self.ids.start_stop.text = "Start Camera"
         self.ids.camera.texture = None
 
     def capture(self):
+        """
+        Captures a picture from the webcam and saves it to filepath with
+        the name of the current time
+        """
         current_time = time.strftime('%Y%m%d-%H%M%S')
         self.filepath = "pictures/" + current_time + ".png"
         self.ids.camera.export_to_png(self.filepath)
@@ -27,18 +34,29 @@ class CameraScreen(Screen):
 
 
 class ImageScreen(Screen):
+    link_message: "Create Link First"
+
     def create_link(self):
+        """Accesses the photo filepath, uploads it to the web, and inserts the
+        link in the Label widget"""
         file_path = App.get_running_app().root.ids.camera_screen.filepath
         fileshare = FileSharer(filepath=file_path)
         self.url = fileshare.share()
         self.ids.link_text.text = str(self.url)
 
     def copy_link(self):
+        """Copies the link in the Label widget to the clipboard"""
         try:
             Clipboard.copy(str(self.url))
         except:
-            self.ids.link_text.text = "Create a Link First"
+            self.ids.link_text.text = self.link_message
 
+    def open_link(self):
+        """Opens the link from the Label widget on the web"""
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.link_text.text = self.link_message
 
 
 class RootWidget(ScreenManager):
